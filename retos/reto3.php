3285b226f5299b9af227faa57687cec9675c21df
aitorarroyo10 <464097.joan23@fje.edu>
2025-03-16 18:38:11 +0100

    Icono añadido

--- retos/reto3.php
+++ retos/reto3.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/vnd.microsoft.icon" href="favicon/bowl-food-solid.png">
     <link rel="stylesheet" href="../css/styles.css">
     <title>Prueba nº3</title>
 </head>
